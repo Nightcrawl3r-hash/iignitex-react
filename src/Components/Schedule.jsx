4726ea089c0a3fc91214e667f49a39c6987977de
Naresh Rajbanshi <nar3sh0@gmail.com>
2023-12-09 17:36:32 +0545

    removed json server and just mapped the data

--- src/Components/Schedule.jsx
+++ src/Components/Schedule.jsx
@@ -1,15 +1,188 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import React, { useState } from "react";
+
+const weeklySchedule = [
+  {
+    day: "Monday",
+    classes: [
+      {
+        className: "Cardio Blast",
+        time: "9:00 AM - 10:00 AM",
+        trainer: "Alex Smith",
+      },
+      {
+        className: "Strength Training",
+        time: "4:00 PM - 5:00 PM",
+        trainer: "Emily Johnson",
+      },
+      {
+        className: "Yoga for Beginners",
+        time: "6:00 PM - 7:00 PM",
+        trainer: "Mark Anderson",
+      },
+      {
+        className: "Core Conditioning",
+        time: "8:00 PM - 9:00 PM",
+        trainer: "Jessica Taylor",
+      },
+    ],
+  },
+  {
+    day: "Tuesday",
+    classes: [
+      {
+        className: "Yoga Fusion",
+        time: "10:30 AM - 11:30 AM",
+        trainer: "Michael Davis",
+      },
+      {
+        className: "HIIT",
+        time: "5:30 PM - 6:30 PM",
+        trainer: "Sarah White",
+      },
+      {
+        className: "Pilates Sculpt",
+        time: "7:30 PM - 8:30 PM",
+        trainer: "Chris Brown",
+      },
+      {
+        className: "Total Body Burn",
+        time: "9:00 PM - 10:00 PM",
+        trainer: "Emma Turner",
+      },
+    ],
+  },
+  {
+    day: "Wednesday",
+    classes: [
+      {
+        className: "Pilates",
+        time: "8:00 AM - 9:00 AM",
+        trainer: "Chris Brown",
+      },
+      {
+        className: "Boxing Fitness",
+        time: "6:00 PM - 7:00 PM",
+        trainer: "Emma Taylor",
+      },
+      {
+        className: "Barre Sculpt",
+        time: "7:30 PM - 8:30 PM",
+        trainer: "David White",
+      },
+      {
+        className: "Stretch and Relax",
+        time: "9:00 PM - 10:00 PM",
+        trainer: "Sophia Adams",
+      },
+    ],
+  },
+  {
+    day: "Thursday",
+    classes: [
+      {
+        className: "Spin Class",
+        time: "7:30 AM - 8:30 AM",
+        trainer: "Daniel Lee",
+      },
+      {
+        className: "Zumba",
+        time: "3:00 PM - 4:00 PM",
+        trainer: "Olivia Martinez",
+      },
+      {
+        className: "Functional Fitness",
+        time: "6:30 PM - 7:30 PM",
+        trainer: "Ryan Wilson",
+      },
+      {
+        className: "Dance Cardio",
+        time: "8:30 PM - 9:30 PM",
+        trainer: "Lily Turner",
+      },
+    ],
+  },
+  {
+    day: "Friday",
+    classes: [
+      {
+        className: "Functional Fitness",
+        time: "11:00 AM - 12:00 PM",
+        trainer: "Ryan Wilson",
+      },
+      {
+        className: "Dance Cardio",
+        time: "7:00 PM - 8:00 PM",
+        trainer: "Sophia Adams",
+      },
+      {
+        className: "Bootcamp",
+        time: "8:30 PM - 9:30 PM",
+        trainer: "Matthew Miller",
+      },
+      {
+        className: "Flexibility Training",
+        time: "10:00 PM - 11:00 PM",
+        trainer: "Lily Turner",
+      },
+    ],
+  },
+  {
+    day: "Saturday",
+    classes: [
+      {
+        className: "Bootcamp",
+        time: "9:30 AM - 10:30 AM",
+        trainer: "Matthew Miller",
+      },
+      {
+        className: "Flexibility Training",
+        time: "2:00 PM - 3:00 PM",
+        trainer: "Lily Turner",
+      },
+      {
+        className: "Meditation",
+        time: "4:00 PM - 5:00 PM",
+        trainer: "Ava Garcia",
+      },
+      {
+        className: "Strength and Balance",
+        time: "6:00 PM - 7:00 PM",
+        trainer: "Noah Clark",
+      },
+    ],
+  },
+  {
+    day: "Sunday",
+    classes: [
+      {
+        className: "Meditation",
+        time: "12:00 PM - 1:00 PM",
+        trainer: "Ava Garcia",
+      },
+      {
+        className: "Bodyweight Circuit",
+        time: "4:30 PM - 5:30 PM",
+        trainer: "Noah Clark",
+      },
+      {
+        className: "Yoga Flow",
+        time: "6:30 PM - 7:30 PM",
+        trainer: "Emma Turner",
+      },
+      {
+        className: "High Intensity Yoga",
+        time: "8:00 PM - 9:00 PM",
+        trainer: "David White",
+      },
+    ],
+  },
+];
 
 const Schedule = () => {
-  const URL = `http://localhost:6900/weeklySchedule`;
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-  const [Data, setData] = useState([]);
   const [selectedDay, setselectedDay] = useState("");
 
   const displaySchedule = (day) => {
-    const daySchedule = Data.find((item) => item.day === day);
+    const daySchedule = weeklySchedule.find((item) => item.day === day);
 
     if (daySchedule) {
       return (
@@ -30,22 +203,6 @@ const Schedule = () => {
       return <p>No classes scheduled for this day.</p>;
     }
   };
-
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        const response = await axios.get(URL);
-        setData(response.data);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-        setError(error);
-      } finally {
-        setLoading(false);
-      }
-    }
-
-    fetchData();
-  }, []);
 
   return (
     <div className="container m-auto py-16 px-2 md:px-0">
@@ -69,18 +226,14 @@ const Schedule = () => {
                   <option value="" disabled>
                     Select a day
                   </option>
-                  {Data.map((item) => (
+                  {weeklySchedule.map((item) => (
                     <option key={item.day} value={item.day}>
                       {item.day}
                     </option>
                   ))}
                 </select>
               </div>
-              <div className="col-span-4">
-                {!loading && !error ? displaySchedule(selectedDay) : null}
-                {loading && <p>Loading...</p>}
-                {error && <p>Error fetching data. Please try again later.</p>}
-              </div>
+              <div className="col-span-4">{displaySchedule(selectedDay)}</div>
             </div>
           </div>
         </div>
